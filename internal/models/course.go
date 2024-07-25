339bdc98bb997f19e01a4df8cc0947c547fb668c
Adam Ou-Yang <adon988@users.noreply.github.com>
2024-07-25 15:14:09 +0800

    feat: origanization permission(json) feature

--- internal/models/course.go
+++ internal/models/course.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Course struct {
-	Id             string       `gorm:"primaryKey;size:24"`
-	Title          string       `gorm:"size:255"`
-	OrganizationId string       `gorm:"size:24;index"`
-	Order          int32        `gorm:"size:10"`
-	PublishStatus  int32        `gorm:"size:10;default:3;comment:'(1 public, 2 member_public, 3 draft)'"`
-	CreaterId      string       `gorm:"size:24;index;comment:'only note the creater id, not the permission'"`
-	Permissions    []Permission `gorm:"type:json"`
+	Id             string `gorm:"primaryKey;size:24"`
+	Title          string `gorm:"size:255"`
+	OrganizationId string `gorm:"size:24;index"`
+	Order          int32  `gorm:"size:10"`
+	PublishStatus  int32  `gorm:"size:10;default:3;comment:'(1 public, 2 member_public, 3 draft)'"`
+	CreaterId      string `gorm:"size:24;index;comment:'only note the creater id, not the permission'"`
+	Permissions    string `gorm:"type:json;column:permissions"`
 	CreatedAt      time.Time
 	UpdatedAt      time.Time
 	DeletedAt      gorm.DeletedAt `gorm:"index"`
--- internal/models/organization.go
+++ internal/models/organization.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Organization struct {
-	Id              string       `gorm:"primaryKey;size:24"`
-	Title           string       `gorm:"size:255"`
-	Order           int32        `gorm:"size:10"`
-	SourceLangeuage string       `gorm:"size:255"`
-	TargetLanguage  string       `gorm:"size:255"`
-	CreaterId       string       `gorm:"size:24;index;comment:'only note the creater id, not the permission'"`
-	Permissions     []Permission `gorm:"type:json"`
+	Id              string `gorm:"primaryKey;size:24"`
+	Title           string `gorm:"size:255"`
+	Order           int32  `gorm:"size:10"`
+	SourceLangeuage string `gorm:"size:255"`
+	TargetLanguage  string `gorm:"size:255"`
+	CreaterId       string `gorm:"size:24;index;comment:'only note the creater id, not the permission'"`
+	Permissions     string `gorm:"type:json;column:permissions"`
 	CreatedAt       time.Time
 	UpdatedAt       time.Time
 	DeletedAt       gorm.DeletedAt `gorm:"index"`
