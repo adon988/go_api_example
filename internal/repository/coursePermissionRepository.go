2aec1f5be4774587371374d0609737aaa3af2af3
Adam Ou-Yang <adon988@users.noreply.github.com>
2024-09-05 23:27:13 +0800

    feat: course permission repository

--- internal/repository/coursePermissionRepository.go
+++ internal/repository/coursePermissionRepository.go
@@ -1,1 +1,107 @@
 package repository
+
+import (
+	"fmt"
+
+	"github.com/adon988/go_api_example/internal/models"
+	"gorm.io/gorm"
+)
+
+type CoursePermissionRepository interface {
+	CreateCoursePermission(coursePermission models.CoursePermission) error
+	GetCoursePermissionByMemberID(member_id string) ([]models.CoursePermission, error)
+	GetCoursePermissionByMemberIDAndCourseID(member_id string, course_id string) (models.CoursePermission, error)
+	UpdateCoursePermission(coursePermission models.CoursePermission) error
+	DeleteCoursePermission(id string) error
+	AssignCoursePermission(coursePermission models.CoursePermission) error
+}
+
+func NewCoursePermissionRepository(db *gorm.DB) CoursePermissionRepository {
+	return &CoursePermissionImpl{DB: db}
+}
+
+type CoursePermissionImpl struct {
+	DB *gorm.DB
+}
+
+func (r *CoursePermissionImpl) CreateCoursePermission(coursePermission models.CoursePermission) error {
+	hasRole := false
+	for _, v := range models.PermissionRoleEnum {
+		if v == coursePermission.Role {
+			hasRole = true
+			break
+		}
+	}
+	if !hasRole {
+		return fmt.Errorf("role not found")
+	}
+	result := r.DB.Create(&coursePermission)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (r *CoursePermissionImpl) GetCoursePermissionByMemberID(member_id string) ([]models.CoursePermission, error) {
+	var coursesPerms []models.CoursePermission
+	result := r.DB.Find(&coursesPerms, "member_id = ?", member_id)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return coursesPerms, nil
+}
+
+func (r *CoursePermissionImpl) GetCoursePermissionByMemberIDAndCourseID(member_id string, course_id string) (models.CoursePermission, error) {
+	var coursePerms models.CoursePermission
+	result := r.DB.Find(&coursePerms, "member_id = ? AND entity_id = ?", member_id, course_id)
+	if result.Error != nil {
+		return coursePerms, result.Error
+	}
+	if result.RowsAffected == 0 {
+		return coursePerms, gorm.ErrRecordNotFound
+	}
+	return coursePerms, nil
+}
+
+func (r *CoursePermissionImpl) UpdateCoursePermission(course_permission models.CoursePermission) error {
+	result := r.DB.Updates(&course_permission)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (r *CoursePermissionImpl) DeleteCoursePermission(id string) error {
+	result := r.DB.Delete(&models.CoursePermission{}, "entity_id = ?", id)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
+	}
+
+	return nil
+}
+
+func (r *CoursePermissionImpl) AssignCoursePermission(coursePermission models.CoursePermission) error {
+	hasRole := false
+	for _, v := range models.PermissionRoleEnum {
+		if v == coursePermission.Role {
+			hasRole = true
+			break
+		}
+	}
+	if !hasRole {
+		return fmt.Errorf("role not found")
+	}
+
+	result := r.DB.Model(&models.CoursePermission{}).Where("member_id = ? AND entity_id = ?", coursePermission.MemberId, coursePermission.EntityId).Updates(&coursePermission)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		result = r.DB.Create(&coursePermission)
+	}
+	return result.Error
+}
--- internal/repository/coursePermissionRepository_test.go
+++ internal/repository/coursePermissionRepository_test.go
@@ -1,1 +1,146 @@
 package repository
+
+import (
+	"testing"
+
+	"github.com/adon988/go_api_example/internal/models"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCoursePermissionRepository_CreateCoursePermission(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.CoursePermission{})
+	repo := NewCoursePermissionRepository(mockDB)
+	coursePerm := models.CoursePermission{
+		MemberId: "1",
+		EntityId: "1",
+		Role:     1,
+	}
+	err := repo.CreateCoursePermission(coursePerm)
+	assert.Nil(t, err)
+	// 檢查 course_permissions 是否被建立
+	var coursePerms []models.CoursePermission
+	mockDB.Find(&coursePerms)
+	assert.Equal(t, 1, len(coursePerms))
+	assert.Equal(t, "1", coursePerms[0].MemberId)
+	assert.Equal(t, "1", coursePerms[0].EntityId)
+	// test role not found
+	coursePerm = models.CoursePermission{
+		MemberId: "1",
+		EntityId: "1",
+		Role:     4,
+	}
+	err = repo.CreateCoursePermission(coursePerm)
+	assert.Equal(t, "role not found", err.Error())
+
+}
+
+func TestCoursePermissionRepository_GetCourseByMemberIDAndCourseID(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.CoursePermission{})
+	repo := NewCoursePermissionRepository(mockDB)
+	coursePerm := models.CoursePermission{
+		MemberId: "1",
+		EntityId: "1",
+		Role:     1,
+	}
+	//creatre
+	err := repo.AssignCoursePermission(coursePerm)
+	assert.Nil(t, err)
+	coursePerms, err := repo.GetCoursePermissionByMemberIDAndCourseID("1", "1")
+	assert.Nil(t, err)
+	assert.Equal(t, "1", coursePerms.MemberId)
+	assert.Equal(t, "1", coursePerms.EntityId)
+	assert.Equal(t, int32(1), coursePerm.Role)
+	//update
+	coursePerm2 := models.CoursePermission{
+		MemberId: "1",
+		EntityId: "1",
+		Role:     2,
+	}
+	err = repo.AssignCoursePermission(coursePerm2)
+	assert.Nil(t, err)
+	coursePerms, err = repo.GetCoursePermissionByMemberIDAndCourseID("1", "1")
+	assert.Nil(t, err)
+	assert.Equal(t, int32(2), coursePerm2.Role)
+}
+
+func TestCoursePermissionRepository_UpdateCoursePermission(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.CoursePermission{})
+	repo := NewCoursePermissionRepository(mockDB)
+	coursePerm := models.CoursePermission{
+		MemberId: "1",
+		EntityId: "1",
+		Role:     1,
+	}
+	mockDB.Create(&coursePerm)
+	coursePerm.Role = 2
+	err := repo.UpdateCoursePermission(coursePerm)
+	assert.Nil(t, err)
+	// 檢查 course_permissions 是否被更新
+	var coursePerms []models.CoursePermission
+	mockDB.Find(&coursePerms)
+	assert.Equal(t, 1, len(coursePerms))
+	assert.Equal(t, int32(2), coursePerms[0].Role)
+}
+
+func TestCoursePermissionRepository_DeleteCoursePermission(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.CoursePermission{})
+	repo := NewCoursePermissionRepository(mockDB)
+	coursePerm := models.CoursePermission{
+		MemberId: "1",
+		EntityId: "1",
+		Role:     1,
+	}
+	mockDB.Create(&coursePerm)
+	err := repo.DeleteCoursePermission("1")
+	assert.Nil(t, err)
+	// 檢查 course_permissions 是否被刪除
+	var coursePerms []models.CoursePermission
+	mockDB.Find(&coursePerms)
+	assert.Equal(t, 0, len(coursePerms))
+}
+
+func TestCoursePermissionRepository_AssignCoursePermission(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.CoursePermission{})
+	repo := NewCoursePermissionRepository(mockDB)
+	coursePerm := models.CoursePermission{
+		MemberId: "1",
+		EntityId: "1",
+		Role:     1,
+	}
+	//create
+	err := repo.AssignCoursePermission(coursePerm)
+	assert.Nil(t, err)
+	// 檢查 course_permissions 是否被建立
+	var coursePerms []models.CoursePermission
+	mockDB.Find(&coursePerms)
+	assert.Equal(t, 1, len(coursePerms))
+	assert.Equal(t, "1", coursePerms[0].MemberId)
+	assert.Equal(t, "1", coursePerms[0].EntityId)
+	// test role not found
+	coursePerm = models.CoursePermission{
+		MemberId: "1",
+		EntityId: "1",
+		Role:     4,
+	}
+	err = repo.AssignCoursePermission(coursePerm)
+	assert.Equal(t, "role not found", err.Error())
+	//update
+	coursePerm2 := models.CoursePermission{
+		MemberId: "1",
+		EntityId: "1",
+		Role:     2,
+	}
+	err = repo.AssignCoursePermission(coursePerm2)
+	assert.Nil(t, err)
+	// 檢查 course_permissions 是否被更新
+	mockDB.Find(&coursePerms)
+	assert.Equal(t, 1, len(coursePerms))
+	assert.Equal(t, int32(2), coursePerms[0].Role)
+}
