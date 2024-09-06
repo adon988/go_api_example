3f48f07f0bffc85fb7062269cd266c2d1df55ebe
Adam Ou-Yang <adon988@users.noreply.github.com>
2024-09-06 15:09:19 +0800

    feat: course service

---
+++ internal/services/courseService.go
@@ -0,0 +1,70 @@
+package services
+
+import (
+	"github.com/adon988/go_api_example/internal/models"
+	"github.com/adon988/go_api_example/internal/repository"
+	"gorm.io/gorm"
+)
+
+type CourseServiceInterface interface {
+	CreateCourseNPermission(member_id string, role int32, course models.Course) error
+	GetCourse(member_id string) ([]models.Course, error)
+	UpdateCourse(course models.Course) error
+	DeleteCourse(id string) error
+	GetCoursePermissionByMemberIDAndCourseID(member_id string, course_id string) (models.CoursePermission, error)
+	AssignCoursePermission(coursePermission models.CoursePermission) error
+}
+
+func NewCourseSerive(db *gorm.DB) CourseServiceInterface {
+	return &CourseService{
+		course:           repository.NewCourseRepository(db),
+		coursePermission: repository.NewCoursePermissionRepository(db),
+	}
+}
+
+type CourseService struct {
+	course           repository.CourseRepository
+	coursePermission repository.CoursePermissionRepository
+}
+
+func (service CourseService) CreateCourseNPermission(member_id string, role int32, course models.Course) error {
+	coursePermission := models.CoursePermission{
+		MemberId: member_id,
+		EntityId: course.Id,
+		Role:     role,
+	}
+
+	result := service.coursePermission.CreateCoursePermission(coursePermission)
+	if result != nil {
+		return result
+	}
+
+	err := service.course.CreateCourse(course)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (service CourseService) GetCourse(member_id string) ([]models.Course, error) {
+	return service.course.GetCourseByMemberID(member_id)
+}
+
+func (service CourseService) UpdateCourse(course models.Course) error {
+	return service.course.UpdateCourse(course)
+}
+
+func (service CourseService) DeleteCourse(id string) error {
+	if err := service.coursePermission.DeleteCoursePermission(id); err != nil {
+		return err
+	}
+	return service.course.DeleteCourse(id)
+}
+
+func (service CourseService) GetCoursePermissionByMemberIDAndCourseID(member_id string, course_id string) (models.CoursePermission, error) {
+	return service.coursePermission.GetCoursePermissionByMemberIDAndCourseID(member_id, course_id)
+}
+
+func (service CourseService) AssignCoursePermission(coursePermission models.CoursePermission) error {
+	return service.coursePermission.AssignCoursePermission(coursePermission)
+}
---
+++ internal/services/courseService_test.go
@@ -0,0 +1,124 @@
+package services
+
+import (
+	"testing"
+
+	"github.com/adon988/go_api_example/internal/models"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCourseService_CreateCourseNPermission(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.Course{}, &models.CoursePermission{})
+
+	memberId := "1"
+	course := models.Course{
+		Id:        "1",
+		Title:     "course title",
+		Order:     1,
+		Publish:   1,
+		CreaterId: memberId,
+	}
+	role := int32(1)
+	service := NewCourseSerive(mockDB)
+	result := service.CreateCourseNPermission(memberId, role, course)
+	assert.Nil(t, result)
+
+}
+
+func TestCourseService_GetCourse(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.Course{}, &models.CoursePermission{})
+
+	memberId := "1"
+	course := models.Course{
+		Id:        "1",
+		Title:     "course title",
+		Order:     1,
+		Publish:   1,
+		CreaterId: memberId,
+	}
+	role := int32(1)
+	service := NewCourseSerive(mockDB)
+	result := service.CreateCourseNPermission(memberId, role, course)
+	assert.Nil(t, result)
+
+	courses, err := service.GetCourse(memberId)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(courses))
+}
+
+func TestCourseService_GetCoursePermissionByMemberIDAndCourseID(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.Course{}, &models.CoursePermission{})
+
+	memberId := "1"
+	course := models.Course{
+		Id:        "1",
+		Title:     "course title",
+		Order:     1,
+		Publish:   1,
+		CreaterId: memberId,
+	}
+	role := int32(1)
+	service := NewCourseSerive(mockDB)
+	result := service.CreateCourseNPermission(memberId, role, course)
+	assert.Nil(t, result)
+
+	coursePerms, err := service.GetCoursePermissionByMemberIDAndCourseID(course.Id, memberId)
+	assert.Nil(t, err)
+	assert.Equal(t, "1", coursePerms.EntityId)
+}
+
+func TestCourseService_UpdateCourse(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.Course{}, &models.CoursePermission{})
+
+	memberId := "1"
+	course := models.Course{
+		Id:        "1",
+		Title:     "course title",
+		Order:     1,
+		Publish:   1,
+		CreaterId: memberId,
+	}
+	role := int32(1)
+	service := NewCourseSerive(mockDB)
+	result := service.CreateCourseNPermission(memberId, role, course)
+	assert.Nil(t, result)
+
+	course.Title = "new course title"
+	err := service.UpdateCourse(course)
+	assert.Nil(t, err)
+
+	courses, err := service.GetCourse(memberId)
+	assert.Nil(t, err)
+	assert.Equal(t, "new course title", courses[0].Title)
+}
+
+func TestCourseService_DeleteCourse(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.Course{}, &models.CoursePermission{})
+
+	memberId := "1"
+	course := models.Course{
+		Id:        "1",
+		Title:     "course title",
+		Order:     1,
+		Publish:   1,
+		CreaterId: memberId,
+	}
+	role := int32(1)
+	service := NewCourseSerive(mockDB)
+	result := service.CreateCourseNPermission(memberId, role, course)
+	assert.Nil(t, result)
+
+	err := service.DeleteCourse(course.Id)
+	assert.Nil(t, err)
+
+	courses, err := service.GetCourse(memberId)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(courses))
+}
