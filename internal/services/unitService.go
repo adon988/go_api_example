45ce12e07ccbc05711568f9cf7d6377948820865
Adam Ou-Yang <adon988@users.noreply.github.com>
2024-09-07 10:57:03 +0800

    feat: unit service

---
+++ internal/services/unitService.go
@@ -0,0 +1,70 @@
+package services
+
+import (
+	"github.com/adon988/go_api_example/internal/models"
+	"github.com/adon988/go_api_example/internal/repository"
+	"gorm.io/gorm"
+)
+
+type UnitServiceInterface interface {
+	CreateUnitNPermission(member_id string, role int32, unit models.Unit) error
+	GetUnit(member_id string) ([]models.Unit, error)
+	UpdateUnit(unit models.Unit) error
+	DeleteUnit(id string) error
+	GetUnitPermissionByMemberIDAndUnitID(member_id string, unit_id string) (models.UnitPermission, error)
+	AssignUnitPermission(unitPermission models.UnitPermission) error
+}
+
+type UnitService struct {
+	unit           repository.UnitRepository
+	unitPermission repository.UnitPermissionRepository
+}
+
+func NewUnitService(db *gorm.DB) UnitServiceInterface {
+	return &UnitService{
+		unit:           repository.NewUnitRepository(db),
+		unitPermission: repository.NewUnitPermissionRepository(db),
+	}
+}
+
+func (service UnitService) CreateUnitNPermission(member_id string, role int32, unit models.Unit) error {
+	unitPermission := models.UnitPermission{
+		MemberId: member_id,
+		EntityId: unit.Id,
+		Role:     role,
+	}
+
+	result := service.unitPermission.CreateUnitPermission(unitPermission)
+	if result != nil {
+		return result
+	}
+
+	err := service.unit.CreateUnit(unit)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (service UnitService) GetUnit(member_id string) ([]models.Unit, error) {
+	return service.unit.GetUnitByMemberID(member_id)
+}
+
+func (service UnitService) UpdateUnit(unit models.Unit) error {
+	return service.unit.UpdateUnit(unit)
+}
+
+func (service UnitService) DeleteUnit(id string) error {
+	if err := service.unitPermission.DeleteUnitPermission(id); err != nil {
+		return err
+	}
+	return service.unit.DeleteUnit(id)
+}
+
+func (service UnitService) GetUnitPermissionByMemberIDAndUnitID(member_id string, unit_id string) (models.UnitPermission, error) {
+	return service.unitPermission.GetUnitPermissionByMemberIDAndUnitID(member_id, unit_id)
+}
+
+func (service UnitService) AssignUnitPermission(unitPermission models.UnitPermission) error {
+	return service.unitPermission.AssignUnitPermission(unitPermission)
+}
---
+++ internal/services/unitService_test.go
@@ -0,0 +1,170 @@
+package services
+
+import (
+	"testing"
+
+	"github.com/adon988/go_api_example/internal/models"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestUnitSerivce_CreateUnitNPermission(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.Unit{}, &models.UnitPermission{})
+
+	memberId := "1"
+	unit := models.Unit{
+		Id:             "1",
+		Title:          "unit title",
+		OrganizationId: "1",
+		CourseId:       "1",
+		Order:          1,
+		Publish:        1,
+		CreaterId:      memberId,
+	}
+	role := int32(1)
+	service := NewUnitService(mockDB)
+	result := service.CreateUnitNPermission(memberId, role, unit)
+	assert.Nil(t, result)
+}
+
+func TestUnitSerivce_GetUnit(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.Unit{}, &models.UnitPermission{})
+
+	memberId := "1"
+	unit := models.Unit{
+		Id:             "1",
+		Title:          "unit title",
+		OrganizationId: "1",
+		CourseId:       "1",
+		Order:          1,
+		Publish:        1,
+		CreaterId:      memberId,
+	}
+	role := int32(1)
+	service := NewUnitService(mockDB)
+	result := service.CreateUnitNPermission(memberId, role, unit)
+	assert.Nil(t, result)
+
+	units, err := service.GetUnit(memberId)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(units))
+}
+
+func TestUnitSerivce_GetUnitPermissionByMemberIDAndUnitID(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.Unit{}, &models.UnitPermission{})
+
+	memberId := "1"
+	unit := models.Unit{
+		Id:             "1",
+		Title:          "unit title",
+		OrganizationId: "1",
+		CourseId:       "1",
+		Order:          1,
+		Publish:        1,
+		CreaterId:      memberId,
+	}
+	role := int32(1)
+	service := NewUnitService(mockDB)
+	result := service.CreateUnitNPermission(memberId, role, unit)
+	assert.Nil(t, result)
+
+	unitPermission, err := service.GetUnitPermissionByMemberIDAndUnitID(memberId, unit.Id)
+	assert.Nil(t, err)
+	assert.Equal(t, memberId, unitPermission.MemberId)
+	assert.Equal(t, unit.Id, unitPermission.EntityId)
+	assert.Equal(t, role, unitPermission.Role)
+}
+
+func TestUnitSerivce_AssignUnitPermission(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.Unit{}, &models.UnitPermission{})
+
+	memberId := "1"
+	unit := models.Unit{
+		Id:             "1",
+		Title:          "unit title",
+		OrganizationId: "1",
+		CourseId:       "1",
+		Order:          1,
+		Publish:        1,
+		CreaterId:      memberId,
+	}
+	role := int32(1)
+	service := NewUnitService(mockDB)
+	result := service.CreateUnitNPermission(memberId, role, unit)
+	assert.Nil(t, result)
+
+	unitPermission := models.UnitPermission{
+		MemberId: memberId,
+		EntityId: unit.Id,
+		Role:     role,
+	}
+	err := service.AssignUnitPermission(unitPermission)
+	assert.Nil(t, err)
+
+	unitPermission, err = service.GetUnitPermissionByMemberIDAndUnitID(memberId, unit.Id)
+	assert.Nil(t, err)
+	assert.Equal(t, memberId, unitPermission.MemberId)
+	assert.Equal(t, unit.Id, unitPermission.EntityId)
+	assert.Equal(t, role, unitPermission.Role)
+}
+
+func TestUnitSerivce_UpdateUnit(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.Unit{}, &models.UnitPermission{})
+
+	memberId := "1"
+	unit := models.Unit{
+		Id:             "1",
+		Title:          "unit title",
+		OrganizationId: "1",
+		CourseId:       "1",
+		Order:          1,
+		Publish:        1,
+		CreaterId:      memberId,
+	}
+	role := int32(1)
+	service := NewUnitService(mockDB)
+	result := service.CreateUnitNPermission(memberId, role, unit)
+	assert.Nil(t, result)
+
+	unit.Title = "new title"
+	err := service.UpdateUnit(unit)
+	assert.Nil(t, err)
+
+	units, err := service.GetUnit(memberId)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(units))
+	assert.Equal(t, "new title", units[0].Title)
+}
+
+func TestUnitSerivce_DeleteUnit(t *testing.T) {
+	mockDB, _ := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	mockDB.AutoMigrate(&models.Unit{}, &models.UnitPermission{})
+
+	memberId := "1"
+	unit := models.Unit{
+		Id:             "1",
+		Title:          "unit title",
+		OrganizationId: "1",
+		CourseId:       "1",
+		Order:          1,
+		Publish:        1,
+		CreaterId:      memberId,
+	}
+	role := int32(1)
+	service := NewUnitService(mockDB)
+	result := service.CreateUnitNPermission(memberId, role, unit)
+	assert.Nil(t, result)
+
+	err := service.DeleteUnit(unit.Id)
+	assert.Nil(t, err)
+
+	units, err := service.GetUnit(memberId)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(units))
+}
